f450c63ce0da7385dc1c0e56ec23cc65521c9ffb
Al Beuscher <abeuscher@dynamicsginal.com>
2019-03-20 23:54:59 -0700

    new nav working but cloogy

--- src/js/app.js
+++ src/js/app.js
@@ -279,22 +279,32 @@ var siteActions = [{
     "element": "side-nav",
     "action": function () {
       var theWrapper = document.getElementById("wrapper");
+      var theHeader = document.getElementById("page-header");
       var theToggle = document.getElementById("toggle-side-nav");
+      var closeButton = document.getElementById("btn-close-sidenav");
       theToggle.addEventListener("click", function (e) {
         e.preventDefault();
+        e.stopPropagation();
         if (document.body.classList.contains("nav-open")) {
           document.body.classList.remove("nav-open");
           theWrapper.removeEventListener("click", closeBody);
+          theHeader.removeEventListener("click", closeBody);
+          closeButton.removeEventListener("click", closeBody);
         } else {
           document.body.classList.add("nav-open");
           theWrapper.addEventListener("click", closeBody);
+          theHeader.addEventListener("click", closeBody);
+          closeButton.addEventListener("click", closeBody);
         }
+
       });
 
       function closeBody(e) {
         e.preventDefault();
         document.body.classList.remove("nav-open");
         theWrapper.removeEventListener("click", closeBody);
+        theHeader.removeEventListener("click", closeBody);
+        closeButton.removeEventListener("click", closeBody);
       }
     }
   },
@@ -680,6 +690,7 @@ var siteActions = [{
           duration: 0
         })
         .setPin(el)
+        .setClassToggle(el,"pos-fixed")
         .addTo(homeController);         
       }     
     }
