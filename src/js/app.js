fa866386b325f183b171677c0f3ba182958c7fa4
Al Beuscher <abeuscher@dynamicsginal.com>
2019-05-10 13:13:29 -0700

    Home page tweaks and blog style additions

--- src/js/app.js
+++ src/js/app.js
@@ -306,6 +306,23 @@ var siteActions = [{
     }
   },
   {
+    "element": "hero-words",
+    "action": function () {
+      var interval = 2500;
+      var wordBucket = document.getElementById("hero-words");
+      pageData.hero_words.push({"word": wordBucket.innerHTML});
+      wordBucket.setAttribute("data-current-index",pageData.hero_words.length);
+      window.setTimeout(changeWord,interval);
+      function changeWord () {
+        var currIndex = parseInt(wordBucket.getAttribute("data-current-index"));
+        var newIndex = pageData.hero_words[currIndex+1] ? currIndex+1 : 0;
+        wordBucket.innerHTML = pageData.hero_words[newIndex].word;
+        wordBucket.setAttribute("data-current-index",newIndex);
+        window.setTimeout(changeWord,interval);
+      }
+    }
+  },
+  {
     "element": "solutions-top-carousel",
     "action": function () {
       var logoGall = new Flickity("#solutions-top-carousel", {
