678fc291530de308478dbc37c7a3b3d92d7ca7b2
Alex Beuscher <alexbeuscher@gmail.com>
2019-12-12 13:45:01 -0800

    Working on customwer stories. Got screwed up branching.

---
+++ src/js/widgets/customer-stories-carousel/index.js
@@ -0,0 +1,40 @@
+var parseHTML = require("../../utils/parse-html");
+var Flickity = require("flickity");
+
+require("flickity-as-nav-for");
+
+var templates = {
+    "customerStorySlide": require("./customer-story-slide.pug"), // Video slide on careers page     
+    "logoSlide" : require("./customer-stories-logo.pug")
+};
+
+function CustomerStoryCarousel(els, c) {
+    var customerBucket = els[0];
+    var cg = new Flickity(customerBucket, {
+        "wrapAround": true,
+        "pageDots": false,
+        "lazyLoad": 6,
+        "autoPlay": 8000,
+        "adaptiveHeight": false
+    });
+    for (i in pageData.customer_stories) {
+        cg.append(parseHTML(templates.customerStorySlide(pageData.customer_stories[i])));
+    }
+    cg.resize();
+    var logoStrip = document.getElementById("customer-stories-logos");
+    var logoCarousel = new Flickity(logoStrip, {
+        "wrapAround": false,
+        "pageDots": false,
+        "lazyLoad": false,
+        "autoPlay": false,
+        "adaptiveHeight": false,
+        "asNavFor":customerBucket,
+        "prevNextButtons":false,
+        "groupCells":true
+    });    
+    for (i in pageData.customer_stories) {
+        logoCarousel.append(parseHTML(templates.logoSlide(pageData.customer_stories[i])));
+    }
+}
+
+module.exports = CustomerStoryCarousel;
