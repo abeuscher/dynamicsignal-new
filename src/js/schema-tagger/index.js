238d0ea8309ad18d96b6b9835ef9c4360efd7399
Alex Beuscher <alexbeuscher@gmail.com>
2020-08-26 23:45:39 -0700

    Small change to side nav'

--- src/js/schema-tagger/index.js
+++ src/js/schema-tagger/index.js
@@ -1,12 +1,17 @@
 function SchemaTagger(els) {
-    els.forEach(el=>{
-        el.setAttribute("itemscope","")
-        el.setAttribute("itemtype","https://schema.org/FAQPage")
+    els.forEach(el => {
+        el.setAttribute("itemscope", "")
+        el.setAttribute("itemtype", "https://schema.org/FAQPage")
         let q = el.querySelectorAll("h3")[0]
         let a = el.querySelectorAll("p")[0]
         let content = el.innerHTML
-        q.setAttribute("itemprop","Question") 
-        a.setAttribute("itemprop","acceptedAnswer") 
+        if (q) {
+            q.setAttribute("itemprop", "Question")
+        }
+        if (a) {
+            a.setAttribute("itemprop", "acceptedAnswer")
+        }
+
     })
 }
 
